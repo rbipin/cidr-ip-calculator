f043e4f95c0330bcd96f08c882ff6ed0fb790741
Bipin Radhakrishnan <hi@bipinr.com>
2021-06-23 18:19:52 -0400

    disable middleware

--- api/src/api.py
+++ api/src/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.custom_auth_middleware import CustomAuthenticationMiddleware
 from routes import healthcheck, ciderIP
@@ -6,7 +6,7 @@ from routes import healthcheck, ciderIP
 app = FastAPI(redoc_url="/")
 
 
-app.add_middleware(CustomAuthenticationMiddleware)
+# app.add_middleware(CustomAuthenticationMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
