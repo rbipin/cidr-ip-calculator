cb338263b23593ca536194dd3a8e620c8f67b380
Bipin Radhakrishnan <bipinradhakrishnan@quickenloans.com>
2021-06-24 15:59:05 -0400

    update heroku push commands

--- api/dockercommands.sh
+++ api/dockercommands.sh
@@ -1,3 +1,9 @@
 docker build -t cidr-api .
 
 docker run --name cidr-api -p 8080:80 -d cidr-api
+
+# to push to heroku master
+# login to heroku
+# heroku git: remote -a cidr-calc-api
+
+git subtree push --prefix api heroku master
